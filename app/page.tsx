4707db09859f1eb1b63a0a5164b35a7c0d6a4c7c
Farewelleovv <m14p10a03@gmail.com>
2026-06-24 16:16:19 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -860,6 +860,7 @@ if (
             <option value="All">All eras</option>
             <option value="Euphoria">Euphoria</option>
             <option value="No School Tomorrow">No School Tomorrow</option>
+            <option value="Kcon Japan">Kcon Japan</option>
             <option value="b2p">Boys Planet</option>
             <option value="Other">Other</option>
           </select>
